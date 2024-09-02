02dc0923c73a3a96e6f9fd48c392e0691c86d284
hamzapatel01 <hpatel80@student.ccc.edu>
2024-09-02 02:47:08 -0500

    Remove constant folder and its contents from tracking

--- src/constant/Api-youtube.jsx
+++
@@ -1,5 +0,0 @@
-export const API_KEY = "AIzaSyCKq_Dpon6ri3ffBWb3mhKA_IF22_WBsTw";
-export const BASE_URL = "https://www.googleapis.com/youtube/v3";
-export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${API_KEY}`;
-export const SEARCH_SUGGESTIONS_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
-export default API_KEY; // default export
